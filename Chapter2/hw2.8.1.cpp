545dfbd73497bae5d1faf53b6ed9dad440332caa
Ramesh Koirala <rkoirala@udel.edu>
2017-06-22 16:05:10 -0400

    hw1. all public variables. easier to understand.

--- Chapter2/hw2.8.1.cpp
+++ Chapter2/hw2.8.1.cpp
@@ -8,48 +8,56 @@
 */
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-class polynomial
+class polynomial 
 {
-public:
-  polynomial(int deg=0): degree_(deg), index(-1) {array_ = new double [degree_+1];} //constructor
-  ~polynomial() {delete [] array_;} //destructor
-  void push(const double& item) {array_[++index]=item;}  
-
-private:
-  int degree_;
-  double* array_;
-  int index;
-  friend ostream& operator<<(ostream& out, const polynomial& poly);
+    public:
+        int index=0; //initial value required.
+        int degree=0;
+        double *coeff_array;  //in future try replacing this with vector.
+        
+        //constructor
+        polynomial(int degree): degree(degree){coeff_array = new double [degree+1];} 
+        
+        //fill empty array with coefficient
+        void push(const double& item) {coeff_array[index++]=item;} 
+                
+        //destructor
+        ~polynomial(){}                
 };
 
-// See http://en.cppreference.com/w/cpp/language/operators for insertion and extraction operator
+//operator overloading to print list of coefficient on the screen.
 ostream& operator<<(ostream& out, const polynomial& poly)
 {
-  int deg = poly.degree_;
-  out << "(";
-  for (int i = 0; i < (deg - 1); ++i)
+  int deg = poly.degree;
+  out << "( ";
+  for (int i = 0; i < deg; ++i)
     {
-    out << poly.array_[i] << ",";
-    }
-  if (deg>0)
-    {
-    out << poly.array_[deg-1];
+    out << poly.coeff_array[i] << ", ";
     }
   out << ")";
   return out;
 }
-
-int main(){
-  int deg=5;
-  double coefficient [] = {1.4, 2.8, 0.5, 4, 11.3};
-  polynomial P(deg);
-  for (int i=0; i<deg; i++)
-    {
-      P.push(coefficient[i]);
+       
+int main() 
+{
+    int deg = 5;
+    double coefficient [] = {1.4, 2.8, 0.5, 4, 11.3};
+    
+    //instantiate a class polynomial with 'deg' degree.
+    polynomial P(deg);
+    
+    //Fill coefficient array with the given coefficient. 
+    for (int i=0; i<=deg; ++i)
+    { 
+        P.push(coefficient[i]);   
     }
-  cout << P << endl;
-  return 0;
+    
+    // Print out the final coefficient array.
+    cout << P << endl; // This magic is done due to operator<<
+    
+    return 0;
 }
